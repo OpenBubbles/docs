a2ff229cf68e31f22b1b79aa1a1075f446dcb1e4
obdocs <jacksonlenor445@proton.me>
2024-08-31 19:59:07 -0400

    edited config for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,16 +16,16 @@ const config = {
     url: 'https://obdocs.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/docs/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'obdocs', // Usually your GitHub org/user name.
     projectName: 'docs', // Usually your repo name.
-    // deploymentBranch: 'deployment',
+    deploymentBranch: 'deployment',
     onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
-
+    trailingSlash: false,
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
     // may want to replace "en" with "zh-Hans".
